dd5a65343256b48f68e43244335d441222cf8126
Bouillaguet Quentin <git@desplandis.fr>
2023-02-28 14:03:33 +0100

    TO REMOVE: experiments with copc.js for las loading
    Testing performance of ept parser.
    OOM on chrome due to memory bug in Las-perf wasm loaded by loaders.gl/LasLoader

---
+++ src/Loader/LASLoader.js
@@ -0,0 +1,21 @@
+class LASLoader {
+    /**
+     * @param {ArrayBuffer} data - The file content to parse
+     * @param {Object} options - Options to give to the parser
+     * @param {Object} options.las - TODO
+     * @param {number?} options.las.skip - TODO
+     * @param {number|string} optons.las.colorDepth - TODO
+     */
+    constructor(data, options) {
+        const lf = undefined;
+    }
+}
+
+class LASHandler {
+    constructor() {
+        this.node = '';
+    }
+
+    push(lasBuffer) {
+    }
+}
