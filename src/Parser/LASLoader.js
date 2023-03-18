5d4e8d199388ca88d3dfb6a36c9f085f384d5593
Bouillaguet Quentin <git@desplandis.fr>
2023-03-18 14:46:09 +0100

    TO REMOVE: At least show some data on simple_viewer

---
+++ src/Parser/LASLoader.js
@@ -0,0 +1,182 @@
+import * as LazPerf from 'laz-perf';
+import { Binary, Las } from 'copc';
+
+/**
+ * @typedef {Object} LasLoaderOptions
+ */
+
+/**
+ * @typedef {Object} LASStruct
+ * @property {Las.Header} header
+ * @property {Las.Vlr[]} vlrs
+ * @property {Las.ExtraBytes[]} eb
+ * @property {string} [wkt]
+ */
+
+/**
+ * @param {function(number, number):Promise.<Uint8Array>} get - Buffer
+ * @returns {Promise<LASStruct>}
+ */
+async function create(get) {
+    const header = Las.Header.parse(await get(0, Las.Constants.minHeaderLength));
+    const vlrs = await Las.Vlr.walk(get, header);
+
+    // Required: LAS 1.4
+    const wktVlr = Las.Vlr.find(vlrs, 'LASF_Projection', 2112);
+    const wkt = wktVlr && Binary.toCString(await Las.Vlr.fetch(get, wktVlr));
+
+    // Optional: LAS 1.4
+    const ebVlr = Las.Vlr.find(vlrs, 'LASF_Spec', 4);
+    const eb = ebVlr ?
+        Las.ExtraBytes.parse(await Las.Vlr.fetch(get, ebVlr)) : [];
+
+    // Optional: LAS 1.4 (classification lookup)
+    // const clVlr = Las.Vlr.find(vlrs, 'LASF_Spec', 0);
+    // const truc = await Las.Vlr.fetch(get, clVlr);
+    // function parseClassification(buffer) {
+    //     if (buffer.byteLength % 16) { throw ('Invalid length'); }
+    //     // uchar ClassNumber,
+    //     // char Description[15];
+    // String.fromCharCode(dv.get16Bytes);
+    // }
+
+    return { header, vlrs, eb, wkt };
+}
+
+/**
+ * Load the memory chunk of compressed point data from [pointDataOffset] to
+ * [pointDataOffset] + [pointDataLength].
+ * @param {function(number, number):Promise<Uint8Array>} get - Buffer
+ * @param {Object} chunk
+ * @param {number} chunk.pointDataOffset - Offset from the beginning of the
+ * buffer
+ * @param {number} chunk.pointDataLength - Length of the compressed point data
+ * @returns {Promise<Uint8Array>}
+ */
+async function loadCompressedPointDataBuffer(
+    get,
+    { pointDataOffset, pointDataLength },
+) {
+    return get(pointDataOffset, pointDataOffset + pointDataLength);
+}
+
+/**
+ * @param {function(number, number):Promise.<Uint8Array>} get
+ * @param {Las.Header} header
+ * @param {Object} chunk
+ * @param {number} chunk.pointCount
+ * @param {number} chunk.pointDataOffset
+ * @param {number} chunk.pointDataLength
+ * @param {LazPerf.LazPerf} [lazPerf]
+ */
+async function loadPointDataBuffer(
+    get,
+    { pointDataRecordFormat, pointDataRecordLength },
+    chunk,
+    lazPerf,
+) {
+    const compressed = await loadCompressedPointDataBuffer(get, chunk);
+
+    const { pointCount } = chunk;
+    return Las.PointData.decompressChunk(
+        compressed,
+        { pointCount, pointDataRecordFormat, pointDataRecordLength },
+        lazPerf,
+    );
+}
+
+/**
+ * @param {function(number, number):Promise.<Uint8Array>} get
+ * @param {LASStruct} las
+ * @param {Object} chunk
+ * @param {number} chunk.pointCount
+ * @param {number} chunk.pointDataOffset
+ * @param {number} chunk.pointDataLength
+ * @param {Object} [options]
+ * @param {LazPerf.LazPerf} [options.lazPerf]
+ */
+async function loadPointDataView(get, las, chunk, options = {}) {
+    const buffer = await loadPointDataBuffer(get, las.header, chunk, options.lazPerf);
+    return Las.View.create(buffer, las.header, las.eb);
+}
+
+/**
+ * @param {ArrayBuffer} arrayBuffer
+ * @param {LasLoaderOptions} [options]
+ * @returns {Promise<Object>}
+ */
+async function parseLAS(arrayBuffer, options = {}) {
+    const bytes = new Uint8Array(arrayBuffer);
+    const header = Las.Header.parse(bytes);
+    const pointData = await Las.PointData.decompressFile(bytes);
+
+    const get = async (/** @type{number} */ begin, /** @type{number} */ end) =>
+        bytes.slice(begin, end);
+    const vlrs = await Las.Vlr.walk(get, header);
+    const ebVlr = Las.Vlr.find(vlrs, 'LASF_Spec', 4);
+    const eb = ebVlr ?
+        Las.ExtraBytes.parse(await Las.Vlr.fetch(get, ebVlr)) : [];
+
+    const view = Las.View.create(pointData, header, eb);
+    const getPosition = ['X', 'Y', 'Z'].map(view.getter);
+
+    const positions = new Float32Array(view.pointCount * 3);
+    const colors = new Uint8Array(view.pointCount * 4);
+    const intensities = new Uint16Array(view.pointCount);
+    const classifications = new Uint8Array(view.pointCount);
+
+    const {
+        scale: [scaleX, scaleY, scaleZ],
+        offset: [offsetX, offsetY, offsetZ],
+    } = header;
+
+    for (let i = 0; i < view.pointCount; i++) {
+        const [x, y, z] = getPosition.map(f => f(i)); // TODO
+        positions[i * 3] = x;
+        positions[i * 3 + 1] = y;
+        positions[i * 3 + 2] = z;
+
+        colors[i * 4] = 0;
+        colors[i * 4 + 1] = 0;
+        colors[i * 4 + 3] = 0;
+        colors[i * 4 + 3] = 1;
+
+        intensities[i] = 1;
+        classifications[i] = 0;
+    }
+
+    return {
+        loader: 'las',
+        loaderData: header,
+        header: {
+            vertexCount: header.pointCount,
+        },
+        attributes: {
+            POSITION: { value: positions, size: 3 },
+            COLOR_0: { value: colors, size: 4 },
+            intensity: { value: intensities, size: 1 },
+            classification: { value: classifications, size: 1 },
+        },
+        topology: 'point-list',
+        mode: 0,
+    };
+}
+
+const Loader = {
+    /**
+     * @param {ArrayBuffer} arrayBuffer
+     * @param {LasLoaderOptions} [options]
+     * @returns {Promise<Object>}
+     */
+    parse: async (arrayBuffer, options) => parseLAS(arrayBuffer, options),
+    /**
+     * @param {ArrayBuffer} arrayBuffer
+     * @param {LasLoaderOptions} [options]
+     * @returns {Promise<Object>}
+     */
+    parseSync: (arrayBuffer, options) => parseLAS(arrayBuffer, options),
+};
+
+export default {
+    LASLoader: Loader,
+};
--- src/Parser/LASParser.js
+++ src/Parser/LASParser.js
@@ -1,186 +1,11 @@
 import * as THREE from 'three';
-import { LASLoader } from '@loaders.gl/las';
+// import { LASLoader } from '@loaders.gl/las';
 import { getMeshBoundingBox } from '@loaders.gl/schema';
 import { Binary, Las } from 'copc';
 
 import * as LazPerf from 'laz-perf';
+import * as Module from './LASLoader';
 
-/** @type{LazPerf.LazPerf|null} */
-let lazPerf = null;
-
-/**
- * @param {function(number, number):Promise.<Uint8Array>} get
- */
-async function create(get) {
-    const header = Las.Header.parse(await get(0, Las.Constants.minHeaderLength));
-    const vlrs = await Las.Vlr.walk(get, header);
-
-    let wkt;
-    const wktVlr = Las.Vlr.find(vlrs, 'LASF_Projection', 2112);
-    if (wktVlr) {
-        wkt = Binary.toCString(await Las.Vlr.fetch(get, wktVlr));
-    }
-
-    let eb;
-    const ebVlr = Las.Vlr.find(vlrs, 'LASF_Spec', 4);
-    if (ebVlr) {
-        eb = Las.ExtraBytes.parse(await Las.Vlr.fetch(get, ebVlr));
-    }
-
-    return { header, vlrs, wkt, eb };
-}
-
-/**
- * @param {function(number, number):Promise.<Uint8Array>} get
- * @param {Las.Header} header - TODO: Minimize
- * @param {Object} chunk
- * @param {number} chunk.pointCount
- * @param {number} chunk.pointDataOffset
- * @param {number} chunk.pointDataLength
- * @param {LazPerf.LazPerf} [lazPerf]
- */
-async function loadPointDataBuffer(
-    get,
-    { pointDataRecordFormat, pointDataRecordLength },
-    { pointCount, pointDataOffset, pointDataLength },
-    lazPerf,
-) {
-    const compressed =
-        await get(pointDataOffset, pointDataOffset + pointDataLength);
-
-    return Las.PointData.decompressChunk(
-        compressed,
-        { pointCount, pointDataRecordFormat, pointDataRecordLength },
-        lazPerf,
-    );
-}
-
-/**
- * @param {function(number, number):Promise.<Uint8Array>} get
- * @param {Object} las
- * @param {Las.Header} las.header
- * @param {Las.ExtraBytes[]=} las.eb
- * @param {Object} chunk
- * @param {number} chunk.pointCount
- * @param {number} chunk.pointDataOffset
- * @param {number} chunk.pointDataLength
- * @param {Object} [options]
- * @param {LazPerf.LazPerf} [options.lazPerf]
- */
-async function loadPointXYZ(get, las, chunk, options = {}) {
-    const buffer = await loadPointDataBuffer(get, las.header, chunk, options.lazPerf);
-    return Las.Extractor.create(las.header, las.eb);
-}
-
-/**
- * @param {function(number, number):Promise.<Uint8Array>} get
- * @param {Object} las
- * @param {Las.Header} las.header
- * @param {Las.ExtraBytes[]=} las.eb
- * @param {Object} chunk
- * @param {number} chunk.pointCount
- * @param {number} chunk.pointDataOffset
- * @param {number} chunk.pointDataLength
- * @param {Object} [options]
- * @param {LazPerf.LazPerf} [options.lazPerf]
- */
-async function loadPointDataView(get, las, chunk, options = {}) {
-    const buffer = await loadPointDataBuffer(get, las.header, chunk, options.lazPerf);
-    return Las.View.create(buffer, las.header, las.eb);
-}
-
-/**
- * @param {function(number, number):Promise.<Uint8Array>} get
- * @param {Object} [options]
- * @param {boolean} [options.fp64]
- * @param {number|'auto'} [options.colorDepth]
- */
-async function parseLAS(get, options = {}) {
-    const las = await create(get);
-    const dataView = await loadPointDataView(get, las, {
-        pointCount: las.header.pointCount,
-        pointDataOffset: las.header.pointDataOffset,
-        pointDataLength: las.header.pointDataRecordFormat,
-    });
-    const getters = Object.fromEntries(
-        Object.keys(dataView.dimensions).map(k => [k, dataView.getter(k)]),
-    );
-
-    // Initialize all
-    const total = las.header.pointCount;
-    const positions = new Float32Array(total * 3);
-    const colors = getters.Red ? new Uint8Array(total * 4) : null;
-    const intensities = new Uint16Array(total);
-    const classifications = new Uint8Array(total);
-
-    const loaderData = las.header;
-    const attributes = {
-        POSITION: { value: positions, size: 3 },
-        COLOR_0: { value: colors, size: 4 },
-        intensity: { value: intensities, size: 1 },
-        classification: { value: classifications, size: 1 },
-    };
-
-    const {
-        scale: [scaleX, scaleY, scaleZ],
-        offset: [offsetX, offsetY, offsetZ],
-    } = las.header;
-
-    for (let i = 0; i < dataView.pointCount; ++i) {
-        // TODO: Scale things
-        positions[i * 3] = getters.X(i) * scaleX + offsetX;
-        positions[i * 3 + 1] = getters.Y(i) * scaleY + offsetY;
-        positions[i * 3 + 2] = getters.Z(i) * scaleZ + offsetZ;
-
-        if (colors) {
-            // TODO: Two bytes color
-            colors[i * 4] = getters.Red(i) / 256;
-            colors[i * 4 + 1] = getters.Green(i) / 256;
-            colors[i * 4 + 2] = getters.Blue(i) / 256;
-
-            colors[i * 4 + 3] = 255;
-        }
-
-        intensities[i] = getters.Intensity(i);
-        classifications[i] = getters.Classification(i);
-    }
-
-    return {
-        loader: 'las',
-        loaderData,
-        header: {
-            vertexCount: las.header.pointCount,
-            boundingBox: getMeshBoundingBox(attributes),
-        },
-        attributes,
-        topology: 'point-list',
-        mode: 0, // GL.POINTS
-    };
-}
-
-/**
- * @param {ArrayBuffer} data
- */
-async function parse2(data) {
-    if (!lazPerf) {
-        // /** @type{Partial<LazPerf.LazPerf>} */
-        // const lazPerfConfig = {
-        //     locateFile(_url, _scriptDirectory) {
-        //         console.log(`url: ${lazPerfURL}`);
-        //         return lazPerfURL;
-        //     },
-        // };
-
-        // lazPerf = await LazPerf.createLazPerf(lazPerfConfig);
-        lazPerf = await LazPerf.createLazPerf();
-    }
-
-    const view = new Uint8Array(data);
-    const get = (/** @type {number} */begin, /** @type {number} */ end) =>
-        Promise.resolve(view.subarray(begin, end));
-
-    return parseLAS(get);
-}
 
 // See this document for LAS format specification
 // https://www.asprs.org/wp-content/uploads/2010/12/LAS_1_4_r13.pdf
@@ -215,16 +40,15 @@ export default {
     parse(data, options = {}) {
         options.in = options.in || {};
         options.out = options.out || {};
-        // return LASLoader.parse(data, {
-        //     las: {
-        //         colorDepth: options.in.colorDepth || 'auto',
-        //         skip: options.out.skip || 1,
-        //     },
-        //     worker: true,
-        //     reuseWorkers: false,
-        //     maxConcurrency: navigator.hardwareConcurrency - 1,
-        // }).then((parsedData) => {
-        return parse2(data).then((parsedData) => {
+        return Module.default.LASLoader.parse(data, {
+            las: {
+                colorDepth: options.in.colorDepth || 'auto',
+                skip: options.out.skip || 1,
+            },
+            worker: true,
+            reuseWorkers: false,
+            maxConcurrency: navigator.hardwareConcurrency - 1,
+        }).then((parsedData) => {
             // attributes
             // attributes.COLOR_0: { value: Uint8Array() } // size: 4
             // attributes.POSITION: { value: Float32Array() } // size: 3
@@ -234,7 +58,7 @@ export default {
             // header.vertexCount = nb of points = loaderData.nbPoint
             // header.boundingBox = Array[2][3] = loaderData.boudingBox
             // loaderData = Header LAS obtenu avec las-perf
-            console.log(parsedData.attributes.POSITION.value[0]);
+            console.log(parsedData.attributes.POSITION.value.length);
             const geometry = new THREE.BufferGeometry();
             geometry.userData = parsedData.loaderData;
             geometry.userData.vertexCount = parsedData.header.vertexCount;
